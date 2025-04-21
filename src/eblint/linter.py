0f0417bcf09103762c2dcc76d3550fb3bc6f4dd1
Leon Boschman <leon.boschman@chalmers.se>
2025-04-21 14:03:22 +0200

    Add DependencyFormatChecker

--- src/eblint/linter.py
+++ src/eblint/linter.py
@@ -1,6 +1,8 @@
 import ast
 import os
+import re
 import sys
+from ast import Store
 from typing import List, NamedTuple, Set
 
 
@@ -125,6 +127,50 @@ class FieldOrderChecker(Checker):
         super().generic_visit(node)
 
 
+class DependencyFormatChecker(Checker):
+    VERSION_FORMAT = r"\d+((\.\d+)*)"
+    PACKAGE_NAME_FORMAT = r"\w*"
+
+    def __init__(
+        self,
+        issue_code,
+        dependency_keywords: List[str] = ["dependencies", "builddependencies"],
+    ):
+        super().__init__(issue_code)
+        self.dependency_keywords = dependency_keywords
+        self.stored_names = {}
+
+    def check_string_format(self, string_node, format):
+        if isinstance(string_node, ast.Name):
+            value_string = self.stored_names[string_node.id].value
+        else:
+            value_string = string_node.value
+
+        if re.fullmatch(format, value_string) is None:
+            self.violations.add(
+                Violation(
+                    string_node,
+                    f"Incorrectly formatted package name/version: {value_string}",
+                )
+            )
+
+    def check_dependency_list(self, node):
+        for child in node.elts:
+            self.check_dependency_tuple(child)
+
+    def check_dependency_tuple(self, node):
+        self.check_string_format(node.elts[0], self.PACKAGE_NAME_FORMAT)
+        self.check_string_format(node.elts[1], self.VERSION_FORMAT)
+
+    def visit_Assign(self, node):
+        for target in node.targets:
+            if target.id in self.dependency_keywords and isinstance(target.ctx, Store):
+                self.check_dependency_list(node.value)
+            else:
+                self.stored_names.update({target.id: node.value})
+        super().generic_visit(node)
+
+
 def main():
     source_path = sys.argv[1]
 
@@ -146,6 +192,10 @@ def main():
             field_names=["easyblock", "name", "versionsuffixer"],
             strict_mode=True,
         )
+    )
+
+    linter.checkers.add(
+        DependencyFormatChecker("D001"),
     )
 
     linter.run(source_path)
