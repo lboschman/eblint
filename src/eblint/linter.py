9a0c184be6d2026aaf440a350b4be9731792f39b
Leon Boschman <leon.boschman@chalmers.se>
2025-04-29 22:32:46 +0200

    Add docstrings to linter.py

--- src/eblint/linter.py
+++ src/eblint/linter.py
@@ -6,11 +6,28 @@ from .checkers import DEFAULT_CHECKERS, Checker
 
 
 class Linter:
+    """A linter interface to run a file through multiple checkers.
+
+    Attributes:
+        checkers: collection of objects that check rules
+    """
+
     def __init__(self, checkers: Union[Checker, Set[Checker]] = set()):
+        """Initiate a linter.
+
+        Args:
+            checkers: the rule checkers to be attached to the linter
+        """
         self.checkers = checkers if not isinstance(checkers, Checker) else {checkers}
 
     @staticmethod
     def print_violations(checker: Checker, filename: str):
+        """Print all the violations found in a single file.
+
+        Args:
+            checker: checker whose violations to print
+            filename: file in which the violations where found
+        """
         for node, message in checker.violations:
             if isinstance(node, ast.expr):
                 print(
@@ -20,7 +37,16 @@ class Linter:
             else:
                 print(f"{filename}:1:0: {checker.issue_code}: {message}")
 
-    def run(self, source_path, cleanup: bool = True):
+    def run(self, source_path: str, cleanup: bool = True):
+        """Run a file through the linter.
+
+        In between files it is important to reset the checkers to their original state.
+        It is possible to omit this, but this might lead to unexpected behaviour.
+
+        Args:
+            source_path: path to the file to be checked
+            cleanup: whether to reset the checkers to a clean state afterwards.
+        """
         with open(source_path, "r") as source_file:
             source_code = source_file.read()
 
@@ -38,6 +64,10 @@ class Linter:
 
 
 def main():
+    """Function for command line interface
+
+    This function is invoked by the `eblint` command.
+    """
     parser = argparse.ArgumentParser(
         prog="eblint", description="A linter for easybuild easyconfig files"
     )
