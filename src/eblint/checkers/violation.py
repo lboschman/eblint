a1385b20f1ffcdb2084c06b66a19c6586ffdc71a
Leon Boschman <leon.boschman@chalmers.se>
2025-04-29 22:57:31 +0200

    Add docstring to violation.py

--- src/eblint/checkers/violation.py
+++ src/eblint/checkers/violation.py
@@ -3,5 +3,11 @@ from typing import NamedTuple
 
 
 class Violation(NamedTuple):
+    """A violation of a rule.
+
+    Attributes:
+        node: the node where the violation happened
+        message: message to display
+    """
     node: ast.AST
     message: str
