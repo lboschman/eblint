fe992c345c0c3c23668d3de02f57c9f1a6922e9e
Leon Boschman <leon.boschman@chalmers.se>
2025-04-29 22:51:44 +0200

    Add docstrings to last_field_checker.py

--- src/eblint/checkers/last_field_checker.py
+++ src/eblint/checkers/last_field_checker.py
@@ -6,18 +6,43 @@ from .violation import Violation
 
 
 class LastFieldChecker(Checker):
-    """Checks that the last field in an eb-config file is a particuler field."""
+    """Checker to ensure the last field in an eb-config file is a particular field.
+
+    Attributes:
+        last_field_name: name of the field that should be last
+        last_visited_field_node: node that was visited last, for housekeeping
+    """
 
     def __init__(self, issue_code: str, last_field_name: str = "moduleclass"):
+        """Create LastFieldChecker.
+
+        Args:
+            issue_code: code associated with this rule
+            last_field_name: field that should be last
+        """
         super().__init__(issue_code)
         self.last_field_name = last_field_name
         self.last_visited_field_node: Optional[ast.Name] = None
 
     def visit_Name(self, node: ast.Name):
+        """Visit a Name node.
+
+        Args:
+            node: node to be visited
+        """
         self.last_visited_field_node = node
         super().generic_visit(node)
 
     def visit_Module(self, node: ast.Module):
+        """Visit and leave a module.
+
+        It is impossible to see if the rule has been met until the module has been
+        completely read.
+        Afterwards, the checker is reset to its original state.
+
+        Args:
+            node: module to be visited.
+        """
         super().generic_visit(node)
         if (
             self.last_visited_field_node is not None
