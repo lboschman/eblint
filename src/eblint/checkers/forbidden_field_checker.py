dc9255c1a62a0bfd6c6d42dc11c45fb51fd0e118
Leon Boschman <leon.boschman@chalmers.se>
2025-05-06 13:23:57 +0200

    Add dosctrings to ForbiddenFieldChecker

--- src/eblint/checkers/forbidden_field_checker.py
+++ src/eblint/checkers/forbidden_field_checker.py
@@ -6,11 +6,27 @@ from .violation import Violation
 
 
 class ForbiddenFieldChecker(Checker):
+    """Checker for fields that should not be mentioned in public EasyConfig files.
+
+    Attributes:
+        forbidden_fields: list of forbidden fields
+    """
     def __init__(self, issue_code: str, forbidden_fields: List[str]):
+        """Initiate ForbiddenFieldChecker.
+
+        Args:
+            issue_code: the issue code the linter associates with these forbidden fields
+            forbidden_fields: list of forbidden fields
+        """
         super().__init__(issue_code)
         self.forbidden_fields = forbidden_fields
 
     def visit_Name(self, node: ast.Name):
+        """Visit a Name node.
+
+        Args:
+            node: node to be visited
+        """
         if node.id in self.forbidden_fields:
             self.violations.add(
                 Violation(
